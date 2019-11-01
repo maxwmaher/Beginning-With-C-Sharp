edbf44ae6a8c86f7ba68a875f996935db63c4bfd
maxwmaher <maxwmaher@gmail.com>
2019-11-01 14:40:44 -0500

    introduction to variables

--- BeginningCSharp/Program.cs
+++ BeginningCSharp/Program.cs
@@ -6,11 +6,17 @@ namespace BeginningCSharp
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("   /|");
-            Console.WriteLine("  / |");
-            Console.WriteLine(" /  |");
-            Console.WriteLine("/___|");
-            Console.WriteLine("Triangle");
+            string characterName = "John";
+            int characterAge;
+            characterAge = 35;
+
+            Console.WriteLine("There once was a man named " + characterName + ".");
+            Console.WriteLine("He was " + characterAge + " years old.");
+
+            characterName = "Mike";
+
+            Console.WriteLine("He really liked the name " + characterName + ".");
+            Console.WriteLine("But didn't like being " + characterAge + ".");
 
             Console.ReadLine();
         }
