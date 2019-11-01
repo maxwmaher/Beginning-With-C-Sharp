ba22e49dfecb65a568b0021c7fce7e0c14eafcc9
maxwmaher <maxwmaher@gmail.com>
2019-11-01 14:54:18 -0500

    introduction to data types

--- BeginningCSharp/Program.cs
+++ BeginningCSharp/Program.cs
@@ -6,17 +6,31 @@ namespace BeginningCSharp
     {
         static void Main(string[] args)
         {
-            string characterName = "John";
-            int characterAge;
-            characterAge = 35;
+            //INTRODUCTION TO VARIABLES
 
-            Console.WriteLine("There once was a man named " + characterName + ".");
-            Console.WriteLine("He was " + characterAge + " years old.");
+            //string characterName = "John";
+            //int characterAge;
+            //characterAge = 35;
 
-            characterName = "Mike";
+            //Console.WriteLine("There once was a man named " + characterName + ".");
+            //Console.WriteLine("He was " + characterAge + " years old.");
 
-            Console.WriteLine("He really liked the name " + characterName + ".");
-            Console.WriteLine("But didn't like being " + characterAge + ".");
+            //characterName = "Mike";
+
+            //Console.WriteLine("He really liked the name " + characterName + ".");
+            //Console.WriteLine("But didn't like being " + characterAge + ".");
+
+
+
+            //INTRODUCTION TO DATA TYPES
+
+            string phrase = "C Sharp Professional Development"; //any phrase
+            char grade = 'A'; //any single character
+            int age = 30;
+            double gpa = 3.2;
+            bool isPracticing = true;
+
+            Console.WriteLine(age);
 
             Console.ReadLine();
         }
