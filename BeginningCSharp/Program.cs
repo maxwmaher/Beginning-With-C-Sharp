4c77e5717d73265f86d22b42b539157253bb08ae
maxwmaher <maxwmaher@gmail.com>
2019-11-01 16:29:22 -0500

    working with numbers

--- BeginningCSharp/Program.cs
+++ BeginningCSharp/Program.cs
@@ -36,16 +36,40 @@ namespace BeginningCSharp
 
             //PRACTICE WITH STRINGS
 
-            Console.WriteLine("Max\nMaher"); //Print Max, then on a new line, print Maher
-            Console.WriteLine("\"Max\"Maher"); //Allows for printing of quotiation marks within the string
-            string phrase = "Max Maher" + " is learning C#";
-            Console.WriteLine(phrase.Length); //Print the length of the phrase
-            Console.WriteLine(phrase.ToUpper()); //Converts phrase to UPPERCASE
-            Console.WriteLine(phrase.ToLower()); //Converts phrase to lowercase
-            Console.WriteLine(phrase.Contains("Max")); //Checks to see if phrase includes specificed parameter
-            Console.WriteLine(phrase[0]); //Returns the character at the provided index
-            Console.WriteLine(phrase.IndexOf("Maher")); //Returns the starting index of the value provided, if contained in the string.  Returns -1 if not contained in the string.
-            Console.WriteLine(phrase.Substring(4, 5)); //Retrieves one part of the string.  Begins at index of first value provided, and goes for the number of characters provided in the second value.  No second value is required if looking to grab the full remainder of the string.
+            //Console.WriteLine("Max\nMaher"); //Print Max, then on a new line, print Maher
+            //Console.WriteLine("\"Max\"Maher"); //Allows for printing of quotiation marks within the string
+            //string phrase = "Max Maher" + " is learning C#";
+            //Console.WriteLine(phrase.Length); //Print the length of the phrase
+            //Console.WriteLine(phrase.ToUpper()); //Converts phrase to UPPERCASE
+            //Console.WriteLine(phrase.ToLower()); //Converts phrase to lowercase
+            //Console.WriteLine(phrase.Contains("Max")); //Checks to see if phrase includes specificed parameter
+            //Console.WriteLine(phrase[0]); //Returns the character at the provided index
+            //Console.WriteLine(phrase.IndexOf("Maher")); //Returns the starting index of the value provided, if contained in the string.  Returns -1 if not contained in the string.
+            //Console.WriteLine(phrase.Substring(4, 5)); //Retrieves one part of the string.  Begins at index of first value provided, and goes for the number of characters provided in the second value.  No second value is required if looking to grab the full remainder of the string.
+
+
+
+            //WORKING WITH NUMBERS
+
+            Console.WriteLine(5 + 8); //Returns sum
+            Console.WriteLine(5 - 8); //Returns difference
+            Console.WriteLine(5 * 8); //Returns product
+            Console.WriteLine(40 / 5); //Returns quotient
+            Console.WriteLine(40 % 5); //Modulus operator.  Returns remainder
+            Console.WriteLine(4 + 2 * 3); //Order of operations applies.  3*2 will be performed first, then 4 will be added to get 10.
+            Console.WriteLine((4 + 2) * 3); //Now the inner parentheses will process first (4+2), times 3 to get 18.
+            Console.WriteLine(5 / 2); //We would expect 2.5 normally, but in C#, when only integer values are provided, an integer is returned.  Returns 2.
+            Console.WriteLine(5 / 2.0); //2.0 is considered a double.  This returns 2.5.
+
+            int num = 6;
+            num++; //increments by one.  Can decrement by one with num--;
+            Console.WriteLine(num);
+            Console.WriteLine(Math.Abs(-40)); //Returns the absolute value of the provided value (in this case, 40).
+            Console.WriteLine(Math.Pow(3, 2)); //Returns the first number taken to the power of the second number.
+            Console.WriteLine(Math.Sqrt(36)); //Returns the square root
+            Console.WriteLine(Math.Max(4, 92)); //Returns the largest of the two numbers provided in the parameters
+            Console.WriteLine(Math.Min(4, 92)); //Returns the smallest of the two numbers provided in the parameters
+            Console.WriteLine(Math.Round(4.32)); //Returns the number rounded to the nearest integer
 
             Console.ReadLine();
         }
