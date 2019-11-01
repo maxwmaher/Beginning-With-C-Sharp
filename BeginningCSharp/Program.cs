5bcb2f4d4c8acf8385586c81db052a1b55f1291a
Jon Klapperich <wastewisegame@gmail.com>
2019-11-01 14:15:41 -0500

    first commit

---
+++ BeginningCSharp/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace BeginningCSharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("   /|");
+            Console.WriteLine("  / |");
+            Console.WriteLine(" /  |");
+            Console.WriteLine("/___|");
+
+            Console.ReadLine();
+        }
+    }
+}
