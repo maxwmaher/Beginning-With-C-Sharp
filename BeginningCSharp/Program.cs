b9420620bb20af17a3ba16bbd1fcf54c152b7eef
maxwmaher <maxwmaher@gmail.com>
2019-11-02 11:10:35 -0500

    switch statements

--- BeginningCSharp/Program.cs
+++ BeginningCSharp/Program.cs
@@ -220,34 +220,77 @@ namespace BeginningCSharp
 
             //BUILDING A BETTER CALCULATOR
 
-            Console.Write("Enter a number: ");
-            double num1 = Convert.ToDouble(Console.ReadLine());
+            //Console.Write("Enter a number: ");
+            //double num1 = Convert.ToDouble(Console.ReadLine());
 
-            Console.Write("Enter Operator: ");
-            string op = Console.ReadLine();
+            //Console.Write("Enter Operator: ");
+            //string op = Console.ReadLine();
 
-            Console.Write("Enter a number: ");
-            double num2 = Convert.ToDouble(Console.ReadLine());
+            //Console.Write("Enter a number: ");
+            //double num2 = Convert.ToDouble(Console.ReadLine());
 
-            if (op == "+")
-            {
-                Console.WriteLine(num1 + num2);
-            } else if (op == "-")
-            {
-                Console.WriteLine(num1 - num2);
-            } else if (op == "*")
-            {
-                Console.WriteLine(num1 * num2);
-            }
-            else if (op == "/")
-            {
-                Console.WriteLine(num1 / num2);
-            } else
-            {
-                Console.WriteLine("Invalid operator");
-            }
+            //if (op == "+")
+            //{
+            //    Console.WriteLine(num1 + num2);
+            //} else if (op == "-")
+            //{
+            //    Console.WriteLine(num1 - num2);
+            //} else if (op == "*")
+            //{
+            //    Console.WriteLine(num1 * num2);
+            //}
+            //else if (op == "/")
+            //{
+            //    Console.WriteLine(num1 / num2);
+            //} else
+            //{
+            //    Console.WriteLine("Invalid operator");
+            //}
+
+
+
+            //SWITCH STATEMENTS
+
+            //Convert numbers to days of the week.  0 is Sunday, 1 is Monday, 2 is Tuesday, etc.
+
+            Console.WriteLine(GetDay(3));
 
             Console.ReadLine();
+        }
+
+        static string GetDay(int dayNum)
+        {
+            string dayName;
+
+            switch (dayNum)
+            {
+                case 0:
+                    dayName = "Sunday";
+                    break;
+                case 1:
+                    dayName = "Monday";
+                    break;
+                case 2:
+                    dayName = "Tuesday";
+                    break;
+                case 3:
+                    dayName = "Wednesday";
+                    break;
+                case 4:
+                    dayName = "Thursday";
+                    break;
+                case 5:
+                    dayName = "Friday";
+                    break;
+                case 6:
+                    dayName = "Saturday";
+                    break;
+                default:
+                    dayName = "Invalid entry";
+                    break;
+            }
+
+            return dayName;
         }
     }
 }
